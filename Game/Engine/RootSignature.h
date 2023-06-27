113c92df529ee004f2d480259931353e6049376d
developeSHG <developeSHG@gmail.com>
2023-06-28 00:39:19 +0900

    [Deferred Rendering] 리소스를 렌더타겟 용도로 일반 리소스를 활용하니 더블버퍼링 현상 발생. - 배리어를 이용

--- Game/Engine/RootSignature.h
+++ Game/Engine/RootSignature.h
@@ -1,12 +1,5 @@
 #pragma once
 
-// [계약서 / 결재]
-
-// CPU [        ]    GPU [        ]
-// 한국 [        ]   베트남 [       ]
-// 외주를 보낸다고 하면 단순 "일만 하라" 라고 보낼 순 없다.
-// 방식, 출처 등등 정보를 알려줘야 하는데 그런 계약서의 역할이 RootSignature 클래스.
-
 class RootSignature
 {
 public:
